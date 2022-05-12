1e04af0e33ba98e1fdbee0cf43f2d28d92b6f2fc
o0Freak0o <1918192086@qq.com>
2022-05-12 23:18:51 +0800

    图的基本结构

---
+++ Graph/Edge.java
@@ -0,0 +1,11 @@
+public class Edge {
+    int weight;
+    Node from;
+    Node to;
+
+    public Edge(int weight,Node from,Node to){
+        this.weight = weight;
+        this.from = from;
+        this.to = to;
+    }
+}
---
+++ Graph/Node.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Node {
+
+    public int value;
+    public int in;
+    public int out;
+    public ArrayList<Node> nexts;
+    public ArrayList<Edge> edges;
+
+    public Node(int value){
+        this.value = value;
+        this.in = 0;
+        this.out = 0;
+        this.nexts = new ArrayList<>();
+        this.edges = new ArrayList<>();
+    }
+
+}
