4917baa4571bd1765420bd777e2e0a84b3b05ef6
o0Freak0o <1918192086@qq.com>
2022-05-12 23:20:10 +0800

    图的构造器（邻接表）

---
+++ Graph/GrahCreator.java
@@ -0,0 +1,25 @@
+public class GrahCreator {
+    public static Graph createGraph(Integer[][] matrix){
+        Graph graph = new Graph();
+        for (int i = 0; i < matrix.length;i++){
+            Integer from = matrix[i][0];
+            Integer to = matrix[i][1];
+            Integer weight = matrix[i][2];
+            if(!graph.nodes.containsKey(from)){
+                graph.nodes.put(from,new Node(from));
+            }
+            if(!graph.nodes.containsKey(to)){
+                graph.nodes.put(to,new Node(to));
+            }
+            Node fromnode = graph.nodes.get(from);
+            Node tonode = graph.nodes.get(to);
+            Edge edge = new Edge(weight,fromnode,tonode);
+            fromnode.nexts.add(tonode);
+            fromnode.out++;
+            tonode.in++;
+            fromnode.edges.add(edge);
+            graph.edges.add(edge);
+        }
+        return graph;
+    }
+}
