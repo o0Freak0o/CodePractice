4ee8f58eec30158d272270b20182b7509b68c3a7
o0Freak0o <1918192086@qq.com>
2022-07-05 22:13:58 +0800

    Greedy -java

--- Greedy/src/MaxMoney.java
+++ Greedy/src/MaxMoney.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class MaxMoney {
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 题目描述：
+ * 输入：正数数组costs，正数数组profits，正数k，正数m
+ * 输出：最后获得的最大钱数
+ * 说明：costs[i]表示i号项目的花费，profits[i]标识i号项目在扣除花费后
+ * 还能挣到的钱（利润),k表示你只能做最多K个项目，m表示你的初始资金。
+ * 贪心策略：首先将所有的任务按照cost从小到大排序生成小顶堆，每次（次数小于K）
+ * 从小顶堆中取出所有cost<m的任务，全部加入到profit大顶堆(按照profit从到小排序)中，
+ * 从大顶堆中取出一个任务，更新m值，再次循环。
+ * */
+
+public class MaxMoney {
+    public static class Node{
+        //profits
+        public int p;
+        //costs
+        public int c;
+        public Node(int p,int c){
+            this.c = c;
+            this.p = p;
+        }
+    }
+    //按照花费最小排序的小根堆
+    public static class MinCostComparator implements Comparator<Node>{
+        @Override
+        public int compare(Node o1, Node o2){
+            return o1.c-o2.c;
+        }
+    }
+    //按照利润最大排序的大根堆
+    public static class MaxProfitComparator implements Comparator<Node>{
+        @Override
+        public int compare(Node o1,Node o2){
+            return o2.p-o1.p;
+        }
+    }
+    public static int findMaximizedCapital(int k,int w,int[] Profits,int[] Costs){
+        PriorityQueue<Node> minCostQ = new PriorityQueue<>(new MinCostComparator());
+        PriorityQueue<Node> maxProfitQ = new PriorityQueue<>(new MaxProfitComparator());
+        for(int i = 0; i < Profits.length; i++){
+            minCostQ.add(new Node(Profits[i],Costs[i]));
+        }
+        for(int i = 0; i < k; i++){
+            while(!minCostQ.isEmpty() && minCostQ.peek().c <= w){
+                maxProfitQ.add(minCostQ.poll());
+            }
+            if(maxProfitQ.isEmpty()){
+                return w;
+            }
+            w += maxProfitQ.poll().p;
+        }
+        return w;
+    }
 }
--- Greedy/src/Meeting.java
+++ Greedy/src/Meeting.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Meeting {
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 问题描述：
+ * 一些项目要占用一个会议室宣讲，会议室不能同时容纳两个项目的宣讲
+ * 给你每个项目开始的时间和结束的时间（给你一个数组，里边是一个个具体的项目
+ * 你来安排宣讲的日程，要求会议室进行的宣讲的场次最多，返回这个最多的宣讲场次
+ * 贪心策略：以结束时间最早的项目开始
+ * */
+
+public class Meeting {
+    public static class program{
+        public int start;
+        public int end;
+
+        public program(int start,int end){
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static class ProgramComparator implements Comparator<program>{
+        @Override
+        public int compare(program o1,program o2){
+            return o1.end-o2.end;
+        }
+    }
+    public static int bestArrange(program[] programs, int timePoint){
+        //根据每个会议结束时间进行排序，结束早的在前
+        Arrays.sort(programs,new ProgramComparator());
+        //最后统计的会议的个数
+        int result = 0;
+        //对排序好的数据依次处理
+        for(int i = 0; i < programs.length; i++){
+            if(timePoint <= programs[i].start){
+                result++;
+                timePoint = programs[i].end;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args){
+
+    }
 }
